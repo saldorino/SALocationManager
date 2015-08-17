9827e254f6e226f499cc345497e3278ddb7dc7fc
Sebastian Aldorino <sebastian.aldorino@enyetech.com>
2015-08-17 02:28:40 -0300

    Finished simple implementation of location updates sample

--- Sample/LocationUpdatesViewController.m
+++ Sample/LocationUpdatesViewController.m
@@ -11,15 +11,85 @@
 
 @implementation LocationUpdatesViewController
 {
-    NSMutableArray *_locationUpdatesListeners;
+    NSMutableArray *_continuousLocationUpdatesListeners;
+    NSMutableArray *_nonContinuousLocationUpdatesListeners;
 }
 
 
 
 #pragma mark IBActions
-- (IBAction)addLocationUpdatesListener:(id)sender
+- (IBAction)addContinuousLocationUpdatesListener:(id)sender
 {
-    SALocationUpdatesListener *listener             = [SALocationUpdatesListener new];
+    SALocationUpdatesListener *listener = [self newLocationUpdatesListener];
+    listener.continuousUpdates          = YES;
+
+    [[SALocationManager defaultManager] startUpdatingLocationWithListener:listener];
+
+    [[self continuousLocationUpdatesListeners] addObject:listener];
+}
+
+- (IBAction)removeAllContinuousLocationUpdatesListeners:(id)sender
+{
+    NSMutableArray *listeners = [self continuousLocationUpdatesListeners].copy;
+
+    for (SALocationUpdatesListener *listener in listeners)
+    {
+        [[SALocationManager defaultManager] stopUpdatingLocationWithListener:listener];
+    }
+}
+
+- (IBAction)addNonContinuousLocationUpdatesListener:(id)sender
+{
+    SALocationUpdatesListener *listener = [self newLocationUpdatesListener];
+
+    [[SALocationManager defaultManager] startUpdatingLocationWithListener:listener];
+
+    [[self nonContinuousLocationUpdatesListeners] addObject:listener];
+}
+
+- (IBAction)removeAllNonContinuousLocationUpdatesListeners:(id)sender
+{
+    NSMutableArray *listeners = [self nonContinuousLocationUpdatesListeners];
+
+    for (SALocationUpdatesListener *listener in listeners)
+    {
+        [[SALocationManager defaultManager] stopUpdatingLocationWithListener:listener];
+    }
+
+    [listeners removeAllObjects];
+}
+
+
+
+#pragma mark Private Properties
+- (NSMutableArray *)continuousLocationUpdatesListeners
+{
+    if (!_continuousLocationUpdatesListeners)
+    {
+        _continuousLocationUpdatesListeners = [NSMutableArray new];
+    }
+
+    return _continuousLocationUpdatesListeners;
+}
+
+- (NSMutableArray *)nonContinuousLocationUpdatesListeners
+{
+    if (!_nonContinuousLocationUpdatesListeners)
+    {
+        _nonContinuousLocationUpdatesListeners = [NSMutableArray new];
+    }
+
+    return _nonContinuousLocationUpdatesListeners;
+}
+
+
+
+#pragma mark Private LocationUpdateListeners helpers
+- (SALocationUpdatesListener *)newLocationUpdatesListener
+{
+    SALocationUpdatesListener *listener = [SALocationUpdatesListener new];
+    listener.desiredHorizontalAccuracy  = 100;
+
     __block SALocationUpdatesListener *weakListener = listener;
 
     listener.onLocationUpdated = ^(CLLocation *location)
@@ -36,35 +106,8 @@
     {
         NSLog(@"%@.onLocationUpdatesStopped(%@)", weakListener, location);
 
-        [[self locationUpdatesListeners] removeObject:weakListener];
+        [[self continuousLocationUpdatesListeners] removeObject:weakListener];
     };
-
-    [[SALocationManager defaultManager] startUpdatingLocationWithListener:listener];
-
-    [[self locationUpdatesListeners] addObject:listener];
-}
-
-
-
-#pragma mark Private Properties
-- (NSMutableArray *)locationUpdatesListeners
-{
-    if (!_locationUpdatesListeners)
-    {
-        _locationUpdatesListeners = [NSMutableArray new];
-    }
-
-    return _locationUpdatesListeners;
-}
-
-
-
-#pragma mark Private LocationUpdateListeners helpers
-- (SALocationUpdatesListener *)newLocationUpdatesListener
-{
-    SALocationUpdatesListener *listener = [SALocationUpdatesListener new];
-
-
 
     return listener;
 }
